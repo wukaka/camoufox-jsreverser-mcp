9b66809a6ae56783f2bbfd6818bc2dfa3bfc5a47
Du Chen <mr.duchen@qq.com>
2026-06-09 20:56:04 +0800

    chore: scaffold js-reverse-firefox project (M1.01)

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};

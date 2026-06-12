618f62e925e2e1aaea777ed63a93db95f8e1d352
Du Chen <mr.duchen@qq.com>
2026-06-12 18:05:12 +0800

    feat(tools/stealth): register inject_stealth_hook + inject_stealth_to_workers (M7.10)

--- src/tools/stealth/index.ts
+++ src/tools/stealth/index.ts
@@ -1,4 +1,6 @@
 import { inject_stealth } from './inject_stealth.js';
+import { inject_stealth_hook } from './inject_stealth_hook.js';
+import { inject_stealth_to_workers } from './inject_stealth_to_workers.js';
 import { list_stealth_features } from './list_stealth_features.js';
 import { list_stealth_presets } from './list_stealth_presets.js';
 import { inject_preload_script } from './inject_preload_script.js';
@@ -6,6 +8,8 @@ import { set_user_agent } from './set_user_agent.js';
 
 export const stealthTools = [
   inject_stealth,
+  inject_stealth_hook,
+  inject_stealth_to_workers,
   list_stealth_features,
   list_stealth_presets,
   inject_preload_script,
